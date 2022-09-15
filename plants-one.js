2ca060a9449895c7dc04e7a09867c6dbd092f1cd
Rio Edwards <rioredwards@gmail.com>
2022-09-15 11:50:41 -0700

    Initial commit. Add plants array in plants-one

---
+++ plants-one.js
@@ -0,0 +1,17 @@
+export const plants = [
+    {
+        name: 'philodendron',
+        light: 'bright-indirect',
+        water: 'low-med',
+    },
+    {
+        name: 'succulent',
+        light: 'bright',
+        water: 'low',
+    },
+    {
+        name: 'fern',
+        light: 'low-light',
+        water: 'low-med',
+    },
+];
